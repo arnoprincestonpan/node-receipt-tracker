eb877d4a2611210750dc9d09683d788e1c281650
pan.arno@gmail.com <pan.arno@gmail.com>
2025-02-19 03:43:05 -0800

    Reverted to focus on using Node as back-end. Used body-parser to use json. Changed paths to /api/v1/ to follow industry practice. GET changed to receive json receipts. POST changed. Categories is not typed correctly causing a crash because of the possibility of null or undefined due to the ternary.

--- src/server.js
+++ src/server.js
@@ -11,45 +11,49 @@ const app = express();
 // make a port to listen to requests
 const port = 3000;
 
-app.set('view engine', 'ejs'); // using EJS for views (templates)
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true})); // using body-parser to parse data
 app.use(express.static('public')); // server static files from 'public' folder (i.e. CSS, JS, etc/)
 
 // create an array of Objects for receipts
 
-const receipts = [
-    {
-        id: uuidv4(), // unique ID
-        name: "Groceries",
-        date: new Date(), // latest date and time
-        categories: ['food'],
-        overallCost: 55.75
-    },
-    {
-        id: uuidv4(),
-        name: "Gas",
-        date: new Date(),
-        categories: ["transporation"],
-        overallCost: 40.00
-    }
-];
+const receipts = [];
+
+// const receipts = [
+//     {
+//         id: uuidv4(), // unique ID
+//         name: "Groceries",
+//         date: new Date(), // latest date and time
+//         categories: ['food'],
+//         overallCost: 55.75
+//     },
+//     {
+//         id: uuidv4(),
+//         name: "Gas",
+//         date: new Date(),
+//         categories: ["transporation"],
+//         overallCost: 40.00
+//     }
+// ];
+
+/* API v1 Routes */
+
+// Read (GET)
+app.get('/api/v1/', (req, res) => {
+    res.json(receipts);
+});
 
 // Create (POST)
-app.post('/receipts', (req, res) => {
+app.post('/api/v1/', (req, res) => {
     const newReceipt = {
         id: uuidv4(),
         name: req.body.name,
         date: new Date(req.body.date),
-        categories: req.body.categories ? req.body.categories.split(',') : [], // handle categories
-        overallCost: parseFloat(req.body.overallCost) // parse cost into float
+        categories: req.body.categories || [], 
+        overallCost: parseFloat(req.body.overallCost),
     };
     receipts.push(newReceipt);
-    res.redirect('/');
-});
-
-// Read (GET)
-app.get('/', (req, res) => {
-    res.render('index', { receipts: receipts })
+    res.status(201).json(newReceipt); 
 });
 
 // Update (PUT)
